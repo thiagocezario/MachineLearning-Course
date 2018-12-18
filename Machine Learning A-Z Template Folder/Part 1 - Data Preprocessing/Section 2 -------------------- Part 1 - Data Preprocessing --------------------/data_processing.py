fb00cff8a662e7d5249d7a2968f197b8bb52ddea
Thiago Cezario <thiagocezario2@gmail.com>
2018-12-18 00:12:30 -0200

    Continuation of tutorial

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_processing.py
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_processing.py
@@ -9,8 +9,29 @@ import numpy as np
 import matplotlib.pyplot as plb
 import pandas as pd
 
+#importing data
 dataset = pd.read_csv('Data.csv')
-independents = dataset.iloc[:, :-1].values
-dependents = dataset.iloc[:, 3].values
-df = pd.DataFrame(independents)
-df2 = pd.DataFrame(dependents)
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+"""
+indep = pd.DataFrame(x)
+dep = pd.DataFrame(y)
+"""
+#missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
+imputer = imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+# Encoding categorical data
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_x = LabelEncoder()
+x[:, 0] = labelencoder_x.fit_transform(x[:, 0])
+
+# Separate in columns with numbers instead of strings
+onehotencoder = OneHotEncoder(categorical_features= [0])
+x = onehotencoder.fit_transform(x).toarray()
+
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
